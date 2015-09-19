62bf45558908000cff143bb56f250b90d0fbe50e
Heather Mahan <heatheramahan@gmail.com>
2015-09-19 13:54:39 -0700

    Make barrier reverse polarities. Pair programming ftw!

--- curl_noise/main.js
+++ curl_noise/main.js
@@ -2,16 +2,19 @@ var sketch = function( p ) {
 
 
   var particles = [];
+  var polarities =[];
   var maxParticles = 2000;
-  var maxSpeed = 1;
-  var barrier = p.createVector(600, 500);
+  var maxSpeed = 2;
+  var barrier;
+  var barrierSize;
   var emitter;
 
 
   p.setup = function() {
     p.createCanvas(window.innerWidth-20, window.innerHeight-20);
-    emitter = p.createVector((window.innerWidth-20)/2,
-                            (window.innerHeight-20)/2);
+    barrier = p.createVector(p.width/2, p.height/2);
+    barrierSize = p.min(p.width - 50, p.height - 50);
+    emitter = p.createVector(p.width/2, p.height/2);
     p.background(0);
 
     // Uncomment the following to initialize the particle field to a grid:
@@ -43,22 +46,23 @@ var sketch = function( p ) {
     p.background(0);
     if (particles.length < maxParticles) {
       particles.push(emitter.copy());
+      polarities.push(1);
     }
     //
     //////////////////////////////////////////////////////////
 
     p.stroke(255, 255, 255, 64);
     p.fill(0, 0, 0, 0);
-    p.ellipse(barrier.x, barrier.y, 300, 300);
+    p.ellipse(barrier.x, barrier.y, barrierSize, barrierSize);
 
     for (var i = 0; i < particles.length; i++) {
       var a = particles[i];
-      wrap(a);
-      var b = a.copy();
       var v = curl(a.x, a.y, p.frameCount).setMag(maxSpeed);
+      v.mult(polarities[i]);
       a.add(v);
+      wrap(a, i);
       p.stroke(255);
-      p.point(b.x, b.y);
+      p.point(a.x, a.y);
     }
   };
 
@@ -89,11 +93,11 @@ var sketch = function( p ) {
     return pot(x, y, t) * ramp(x, y);
   }
 
-  
+
   function ramp(x, y) {
     var v = barrier.copy();
     v.sub(x, y);
-    var d = v.mag()-150;
+    var d = v.mag()-barrierSize/2;
     var L = 0.001;
     var r = d/L;
     if (r >= 1) { return 1; }
@@ -101,14 +105,13 @@ var sketch = function( p ) {
     return (15/8)*r - (10/8)*Math.pow(r, 3) + (3/8)*Math.pow(r, 5);
   }
 
-  function wrap(v) {
-    var xOffset = 0,
-        yOffset = 0;
-    if (v.x < 0) { xOffset = p.width; }
-    else if (v.x > p.width) { xOffset = -p.width; }
-    if (v.y < 0) { yOffset = p.height; }
-    else if (v.y > p.height) { yOffset = -p.height; }
-    v.add(p.createVector(xOffset, yOffset));  
+  function wrap(v, i) {
+    var d = p5.Vector.sub(barrier, v);
+    if (d.mag() >= barrierSize/2) {
+      d.setMag(maxSpeed + 1);
+      v.add(d);
+      polarities[i] *= -1;
+    }
   }
 };
 
